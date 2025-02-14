e0b5af349a60756e97baa1cdb9f253669cf17099
Lady 4nna <221annacarolineb@gmail.com>
2025-02-14 09:16:49 -0300

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>parabéns</title>
+    <link rel="stylesheet" href="estilos_destino.css">
+</head>
+<body>
+
+    <h1>Parabéns, você ganhou o cargo de mulher da minha vida!</h1><br>
+    <h2>  Pontos positivos</h2> 
+    <p> Sou muitooooo carinhosa :D</p>
+    <p> Vou te mimar muito ^^</p>
+    <p> Vamo viver muitas loucuras juntas</p>
+    <p> Sou companheira pra tudo ;D</p>
+    <p> Vamos ser muitooo felizes </p>
+    <p> Vamos realizar nossos maiores sonhos</p>
+    <p> Vou fazer varias peças de ceramica pra nossa futura casa ^^ </p>
+    <p> Você nunca vai precisar ter medo de se divertir, porque eu vou sempre te proteger</p>
+    <p> Serei sempre babona por você <3 </p> 
+     <h3> extra: </h3>
+    <p> EU TE AMO! </p>   
+    <img src="imagem_teamo.jpg" alt="te amo"/>
+    <p>img: 10 de janeiro de 2025</p>
+
+</body>
+</html>
